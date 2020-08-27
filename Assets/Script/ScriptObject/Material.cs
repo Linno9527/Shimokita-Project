5ec01e6d3b10ae7cc5c8b32d4cf85d949e59fc79
林诺 <38618940+Linno9527@users.noreply.github.com>
2020-08-27 21:38:36 +0800

    UI已建立
    开始写功能了

---
+++ Assets/Script/ScriptObject/Material.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Materal",menuName = "MyItem/Materal")]
+public class Material : ScriptableObject
+{
+    public string MaterialName;
+    public string MaterialCount;
+    [TextArea]
+    public string MaterialInfo;
+}
